a02d94eb601bf462199dd37351798dfa6c684e28
Aleksander Igras <aigras@aleksander.igras97@gmail.com>
2019-05-24 16:22:06 +0200

    add CFB mode tests using NIST vectors

---
+++ algorithms/tripleDES_InvPermTest.py
@@ -0,0 +1,34 @@
+import tripleDES
+import re
+import os
+
+file = open('//home//aigras//crypto//OpenPGP//algorithms//KAT_TDES//TCFB64invperm.rsp')
+keys = [0,0,0]
+IV = 0
+count = 0
+plain = 0
+cipher = 0
+print("Inverse Permutation test")
+for line in file:
+    if re.match(r'KEYs', line) != None:
+        for i in range(3):
+            keys[i] = int("0x"+line[7:23], 16)
+    if re.match(r'IV', line) != None:
+        IV = int("0x"+line[5:21], 16)
+    if re.match(r'PLAINTEXT', line):
+        plain = int("0x" + line[12:29], 16)
+    if re.match(r'CIPHERTEXT', line):
+        cipher = int("0x" + line[13:30], 16)
+        if(count < 64):
+            out = tripleDES.CFBEncrypt(plain.to_bytes(8, "big"), keys, IV, False)
+        else:
+            out = tripleDES.CFBDecrypt(cipher.to_bytes(8, "big"), keys, IV, False)
+        #print(hex(IV))
+        #print(hex(plain))
+        #print(hex(cipher))
+        #print(hex(keys[0]))
+        #print(count)
+        print(out)
+        count += 1    
+        
+file.close()
---
+++ algorithms/tripleDES_VarTextTest.py
@@ -0,0 +1,47 @@
+import tripleDES
+import re
+import os
+
+file = open('//home//aigras//crypto//OpenPGP//algorithms//KAT_TDES//TCFB64vartext.rsp')
+keys = [0,0,0]
+IV = 0
+count = 0
+plain = 0
+cipher = 0
+print("Variable Text Known Answer Test")
+for line in file:
+    if count < 64:    
+        if re.match(r'KEYs', line) != None:
+            for i in range(3):
+                keys[i] = int("0x"+line[7:23], 16)
+        if re.match(r'IV', line) != None:
+            IV = int("0x"+line[5:21], 16)
+        if re.match(r'PLAINTEXT', line):
+            plain = int("0x" + line[12:29], 16)
+        if re.match(r'CIPHERTEXT', line):
+            cipher = int("0x" + line[13:30], 16)
+            out = tripleDES.CFBEncrypt(plain.to_bytes(8, "big"), keys, IV, False)
+            x = 0
+            for i in out:
+                x = (x << 8) | i
+            print(x == cipher)
+            count += 1
+    else:
+        if re.match(r'KEYs', line) != None:
+            for i in range(3):
+                keys[i] = int("0x"+line[7:23], 16)
+        if re.match(r'IV', line) != None:
+            IV = int("0x"+line[5:21], 16)
+        if re.match(r'CIPHERTEXT', line):
+            cipher = int("0x" + line[13:30], 16)
+        if re.match(r'PLAINTEXT', line):
+            plain = int("0x" + line[12:29], 16)
+            out = tripleDES.CFBDecrypt(cipher.to_bytes(8, "big"), keys, IV, False)
+            x = 0
+            for i in out:
+                x = (x << 8) | i
+            print(x == plain)   
+            count += 1
+        
+        
+file.close()
