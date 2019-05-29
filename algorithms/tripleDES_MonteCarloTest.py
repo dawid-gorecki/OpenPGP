9b2757b50c75f9776b465d033eb06fa3f914d8f6
Aleksander Igras <aigras@aleksander.igras97@gmail.com>
2019-05-29 19:41:42 +0200

    Fix Monte-Carlo test

--- algorithms/tripleDES_MonteCarloTest.py
+++ algorithms/tripleDES_MonteCarloTest.py
@@ -1,53 +1,80 @@
 import tripleDES
 import re
 import os
-print(os.getcwd())
-file = open('.//tdesmct/TCFB64Monte3.rsp')
+import math
+file = open('./algorithms/tdesmct/TCFB64Monte3.rsp')
 keys = [0,0,0]
 IV = 0
 count = 0
 plain = 0
 cipher = 0
-print("Variable Key Known Answer Test")
+initializedEncrypt = False
+initializedDecrypt = False
+print("Monte-Carlo Test")
 for line in file:
     if count < 400:    
-        if re.match(r'KEY1', line) != None:
-            keys[0] = int("0x"+line[7:23], 16)
-        if re.match(r'KEY2', line) != None:
-            keys[1] = int("0x"+line[7:23], 16)
-        if re.match(r'KEY3', line) != None:
-            keys[2] = int("0x"+line[7:23], 16)
-        if re.match(r'IV', line) != None:
-            IV = int("0x"+line[5:21], 16)
-        if re.match(r'PLAINTEXT', line):
-            plain = int("0x" + line[12:29], 16)
+        if not initializedEncrypt:
+            if re.match(r'KEY1', line) != None:
+                keys[0] = int("0x"+line[7:23], 16)
+            if re.match(r'KEY2', line) != None:
+                keys[1] = int("0x"+line[7:23], 16)
+            if re.match(r'KEY3', line) != None:
+                keys[2] = int("0x"+line[7:23], 16)
+            if re.match(r'IV', line) != None:
+                IV = int("0x"+line[5:21], 16)
+            if re.match(r'PLAINTEXT', line):
+                plain = int("0x" + line[12:29], 16)
+                initialized = True
+
         if re.match(r'CIPHERTEXT', line):
             cipher = int("0x" + line[13:30], 16)
             x = 0
+            c = []
             for i in range(10000):
                 out = tripleDES.CFBEncrypt(plain.to_bytes(8, "big"), keys, IV, False)
                 x = 0
                 for i in out:
                     x = (x << 8) | i
+                c.append(x)
                 plain = IV
                 IV = x
-            print(x == cipher)
+            
+            for i in range(3):
+                keys[i] = keys[i] ^ c[9999-i]
+            
+            IV = x
+            if(x != cipher):
+                print("test nr " + str(count) + " failed!")
             count += 1
     else:
-        if re.match(r'KEYs', line) != None:
-            for i in range(3):
-                keys[i] = int("0x"+line[7:23], 16)
-        if re.match(r'IV', line) != None:
-            IV = int("0x"+line[5:21], 16)
-        if re.match(r'CIPHERTEXT', line):
-            cipher = int("0x" + line[13:30], 16)
+        if not initializedDecrypt:
+            if re.match(r'KEY1', line) != None:
+                keys[0] = int("0x"+line[7:23], 16)
+            if re.match(r'KEY2', line) != None:
+                keys[1] = int("0x"+line[7:23], 16)
+            if re.match(r'KEY3', line) != None:
+                keys[2] = int("0x"+line[7:23], 16)
+            if re.match(r'IV', line) != None:
+                IV = int("0x"+line[5:21], 16)
+            if re.match(r'CIPHERTEXT', line):
+                cipher = int("0x" + line[13:30], 16)
+                initializedDecrypt = True
         if re.match(r'PLAINTEXT', line):
             plain = int("0x" + line[12:29], 16)
-            out = tripleDES.CFBDecrypt(cipher.to_bytes(8, "big"), keys, IV, False)
             x = 0
-            for i in out:
-                x = (x << 8) | i
-            print(x == plain)   
+            c = []
+            for i in range(10000):
+                out = tripleDES.CFBDecrypt(cipher.to_bytes(8, "big"), keys, IV, False)
+                x = 0
+                for i in out:
+                    x = (x << 8) | i
+                IV = cipher
+                cipher = x ^ cipher
+                c.append(x)
+            for i in range(3):
+                    keys[i] = keys[i] ^ c[9999-i]
+            if(x != plain):
+                print("test nr " + str(count - 400) + " failed!")
             count += 1
         
         
--- algorithms/tripleDES_test.py
+++ algorithms/tripleDES_test.py
@@ -8,7 +8,7 @@ keys = [
 IV = 0xf69f2445df4f9b17
 
 plaintext = 0x6BC1BEE22E409F96E93D7E117393172AAE2D8A571E03AC9C9EB76FAC45AF8E51
-out = tripleDES.CFBEncrypt(plaintext.to_bytes(32, "big"), keys, IV)
+out = tripleDES.CFBEncrypt(plaintext.to_bytes(32, "big"), keys, IV, False)
 ciphertext = 0x078BB74E59CE7ED67666DE9CF95EAF3FE9ED6BB460F451528A5F9FE4ED710918 
 y = 0
 for i in range(len(out)):
@@ -17,7 +17,7 @@ print(hex(y))
 print(hex(ciphertext))
 print(y == ciphertext)
 
-out = tripleDES.CFBDecrypt(y.to_bytes(32, "big"), keys, IV)
+out = tripleDES.CFBDecrypt(y.to_bytes(32, "big"), keys, IV, False)
 y = 0
 for i in range(len(out)):
     y = (y << 8) | out[i]
