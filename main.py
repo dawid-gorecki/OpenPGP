9ec7c96a944d3cf92d4bb43c09b5ef4b6eea1c18
Dawid Górecki <goreckidawid@outlook.com>
2019-05-31 20:05:54 +0200

    Added user interface for decryption.

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ def main():
     parser.add_argument('-o', "--outfile", required=False, help='Output file.', type=str)
     parser.add_argument('--key', required=False, help='File containing key.', type=str)
     parser.add_argument('--infile', required=True, help='Input file.', type=str)
-    parser.add_argument('--verify', action='store_true')
-    parser.add_argument('--list-packets', action='store_true')
-    parser.add_argument('--sign', action='store_true')
+    parser.add_argument('--verify', action='store_true', help='Verify message.')
+    parser.add_argument('--list-packets', action='store_true', help='List packets in file.')
+    parser.add_argument('--sign', action='store_true', help='Sign data file.')
+    parser.add_argument('--decrypt', action='store_true', help='Decrypt PGP file.')
+    parser.add_argument('--unpack', action='store_true', help='Unpack message in PGP packet.')
     args = parser.parse_args()
     if args.verify:
         if args.key is None:
@@ -39,6 +41,26 @@ def main():
             key_msg.open_pgp_file(args.key)
             msg.sign_message(key_msg)
             msg.write_gpg_file(args.outfile)
+    elif args.decrypt:
+        if args.key is None:
+            print('Please provide a key.')
+        elif args.outfile is None:
+            print('Please provide output file name.')
+        else:
+            msg = PGPMessage()
+            msg.open_pgp_file(args.infile)
+            key_msg = PGPMessage()
+            key_msg.open_pgp_file(args.key)
+            out_msg = PGPMessage()
+            out_msg.from_bytes(msg.decrypt_message(key_msg))
+            out_msg.write_gpg_file(args.outfile)
+    elif args.unpack:
+        if args.outfile is None:
+            print('Please provide output file name.')
+        else:
+            msg = PGPMessage()
+            msg.open_pgp_file(args.infile)
+            msg.unpack_message(args.outfile)
 
 if __name__=='__main__':
     main()
